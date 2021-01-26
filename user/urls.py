40456a61506b6767c719f08bc51107b076a28e9b
sasikala sivaraman <72785420+sasikala07@users.noreply.github.com>
2021-01-26 19:29:13 +0530

    Create urls.py
    first

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from.views import loginpage  ##import the views of loginpage
+from.views import registerpge  ##import the views registerpage
+from django.urls import path
+
+
+urlpatterns = [
+     path('login',loginpage),  ##loginpage is the func in the views.py
+     path('signup',registerpge)
+]
