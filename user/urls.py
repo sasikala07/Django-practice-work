58e6fc02a0a4bb19c20cd930f963f320d169da4e
sasikala sivaraman <72785420+sasikala07@users.noreply.github.com>
2021-01-28 12:38:11 +0530

    Add new code

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
-from.views import loginpage  ##import the views of loginpage
-from.views import registerpge  ##import the views registerpage
+from.views import loginpage
+from.views import registerpge,contactpage
 from django.urls import path
 
 
 urlpatterns = [
      path('login',loginpage),  ##loginpage is the func in the views.py
-     path('signup',registerpge)
+     path('signup',registerpge),
+     path('contact',contactpage)
 ]
--- user/views.py
+++ user/views.py
@@ -2,10 +2,31 @@ from django.shortcuts import render
 ### displayed the data into the browser here
 
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+from.models import Contact
+
+
 def loginpage(request):
     print('Inside login')
     return HttpResponse("<h1 style =color:red>This is login page</h1>")
 
 def registerpge(request):
     return HttpResponse("This is register page")
+
+
+def contactpage(request):
+    if request.method=='GET':
+        template = loader.get_template("contact.html")
+        return HttpResponse(template.render({}, request))
+    else :
+
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        phone = request.POST['phone']
+        email = request.POST['email']
+##django shell code
+        cont = Contact(first_name=fname, last_name=lname, phone=phone, email=email)
+        print (cont)
+        cont.save()
+        return HttpResponse("Saved your details")
