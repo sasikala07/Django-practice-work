94d9b77bcca4f650abfb558fab9731c1478c4f2c
sasikala sivaraman <72785420+sasikala07@users.noreply.github.com>
2021-02-03 11:40:13 +0530

    second commit

--- restapi/restapp/urls.py
+++ restapi/restapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', include('profileapp.urls'))
+    path ('', include('profileapp.urls')),
+    path ('api/', include('todoapp.urls'))
 ]
 
