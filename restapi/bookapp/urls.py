7a6969a183a4d455fdfb82db9ea121179782cea1
sasikala sivaraman <72785420+sasikala07@users.noreply.github.com>
2021-02-05 11:20:54 +0530

    first commit

---
+++ restapi/bookapp/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from .views import AuthorView, PublisherView, BookView,UserCreater
+from rest_framework.routers import DefaultRouter
+
+author_router = DefaultRouter()
+author_router.register('author', AuthorView)
+
+pub_router = DefaultRouter()
+pub_router.register('pub', PublisherView)
+
+book_router = DefaultRouter()
+book_router.register('book', BookView)
+
+
+urlpatterns = [
+   path('', include(pub_router.urls)),
+   path('', include(book_router.urls)),
+   path ('', include(author_router.urls)),
+   path ('account/register', UserCreater.as_view())
+
+    ]
